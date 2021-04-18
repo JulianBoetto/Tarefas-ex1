0ca22aa3703403f050865d3a32c42d90b3425293
julián Boetto <malostratoscba@gmail.com>
2021-04-18 15:59:32 -0300

    Feito post, put e delete

--- index.js
+++ index.js
@@ -2,20 +2,72 @@ const express = require('express')
 const app = express()
 const { Sequelize, DataTypes } = require('sequelize');
 
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'));
+
+const TaskModel = require('./models/task')
+
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: 'tarefas.db'
-  });
+});
 
-app.get('/test', (req, res) => {
-  res.send('Hi!')
+app.use(express.json())
+
+const tasks = TaskModel(sequelize, DataTypes)
+
+app.get('/tasks', async (req, res) => {
+  const tasksAll = await tasks.findAll();
+
+  res.json({tasksAll})
 })
 
-app.post('/tasks', (req, res) => {
-    const body = req.body
+app.post('/tasks', async (req, res) => {
+  const body = req.body
+  const tarefa =  await tasks.create({
+      description: body.description,
+      done: body.done
+  })
 
-    res.json(body)
-    res.end('Post')
+  res.json({ tarefa })  
 })
 
-app.listen(3000, () => console.log('Servidor pronto!'))
+
+app.get('/tasks/:id', async (req, res) => {
+  const taskID = req.params.id;
+  const resID = await tasks.findByPk(taskID);
+
+  res.json(resID);
+})
+
+
+app.put('/tasks/:id', async (req, res) => {
+  const taskID = req.params.id;
+  const body = req.body;
+  const resID = await tasks.findByPk(taskID);
+
+  resID.update({
+    description: body.description,
+    done: body.done
+  })
+
+  res.json({ resID })
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+  const taskID = req.params.id;
+  const resID = await tasks.findByPk(taskID);
+  try {
+    resID.destroy({ where: { id: taskID }})
+    res.send(`Foi eliminado o id: ${ taskID }`);
+  } catch {
+    res.send(`O id: ${ taskID }, não foi encontrado.`)
+  }
+  
+})
+
+
+app.listen(3000, () => {
+  console.log('Servidor pronto!')
+})
